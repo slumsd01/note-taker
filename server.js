510d0f0c4a8f9fe246181f9019f6d0d6af425f46
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2021-12-18 20:57:41 -0500

    display api/notes

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const { json } = require('express/lib/response');
+const router = express.Router();
+const fs = require('fs');
 const path = require('path')
+const notes = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,6 +16,11 @@ app.get('/notes', (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+// api routes
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+})
 
 // port connection
 app.listen(PORT, () => {
