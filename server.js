0db05afd4de40d133b7fe7fd10b9e71ae3349ce8
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2021-12-18 21:37:53 -0500

    add new notes

--- server.js
+++ server.js
@@ -1,28 +1,48 @@
 const express = require('express');
 const { json } = require('express/lib/response');
-const router = express.Router();
 const fs = require('fs');
-const path = require('path')
-const notes = require('./db/db.json');
+const path = require('path');
+const uniqid = require('uniqid'); 
+const database = require('./db/db')
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // html routes
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
 // api routes
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-})
+app.route('/api/notes')
+    .get((req, res) => {
+        res.json(database)
+    })
+    .post((req, res) => {
+        let JSONfile = path.join(__dirname, './db/db.json')
+        let newNote = req.body
+        newNote.id = uniqid();
+
+        database.push(newNote)
+
+        fs.writeFile(JSONfile, JSON.stringify(database), (err) => {
+            if (err) {
+                return console.log(err)
+            }
+            console.log('New note was saved.')
+        })
+        res.json(newNote)
+    })
 
 // port connection
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+    console.log('App listening on PORT ' + PORT);
 });
